7ea91244c8ccd393459da4eab1dace9fd61880f6
xusp <xusp@weizhenzhendeiMac-2.local>
2019-08-09 17:15:37 +0800

    修改细节

--- 05Vue/03mall/src/views/Home.vue
+++ 05Vue/03mall/src/views/Home.vue
@@ -7,6 +7,13 @@
 <script>
 // @ is an alias to /src
 export default {
-  name: 'home',
-}
+  name: "home",
+  async created() {
+    const res = await this.$http.get("/api/goods");
+    const { code, list } = res.data;
+    if (code === 0) {
+      console.log(list);
+    }
+  }
+};
 </script>
