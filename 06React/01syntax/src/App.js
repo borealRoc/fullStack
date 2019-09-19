546305eb9d335d88c08012406201b8b8e7d0ecb6
xusp <xusp@weizhenzhendeiMac-2.local>
2019-09-19 17:15:35 +0800

    react父子组件通讯

--- 06React/01syntax/src/App.js
+++ 06React/01syntax/src/App.js
@@ -9,7 +9,7 @@ let Welcome = props => {
     return <strong>这是一个{props.txt}</strong>
 }
 
-export default class  extends Component {
+export default class extends Component {
 
     formatName(name) {
         return name.firstName + name.lastName
@@ -21,6 +21,12 @@ export default class  extends Component {
         this.state = {
             count: 0,
             time: new Date(),
+            componentWillMount: 'componentWillMount: 组件将要挂载：此时可以访问属性和状态了，可以进行api调用，但没办法做dom相关操作',
+            componentDidMount: 'componentDidMount: 组件已经挂载：可进行状态更新操作',
+            componentWillReceiveProps: 'componentWillReceiveProps: 组件属性更新：父组件传递的属性有变化，做相应响应',
+            shouldComponentUpdate: 'shouldComponentUpdate: 组件是否应该更新？返回布尔值，优化点',
+            componentWillUpdate: 'componentWillUpdate: 组件将要更新',
+            componentDidUpdate: 'componentDidUpdate: 组件已经更新',
         }
     }
 
@@ -37,9 +43,9 @@ export default class  extends Component {
         this.setState((preState, preProps) => ({
             count: preState.count + 1
         }), () => {
-            console.log('在setState的回调函数里，正确取到了值',this.state.count)
+            console.log('在setState的回调函数里，正确取到了值', this.state.count)
         })
-        console.log('因为setState()是异步的,所以我这里取到的值是错误的',this.state.count)
+        console.log('因为setState()是异步的,所以我这里取到的值是错误的', this.state.count)
     }
 
     componentWillUnmount() {
@@ -63,7 +69,7 @@ export default class  extends Component {
                         <h2>2. 属性传递: 动态属性是一个表达式</h2>
                         <p>2.1、动态插值：{name}</p>
                         <p>2.2、动态属性：<img src={logo} className='logo' alt='react logo'></img></p>
-                        <p>2.3、函数方法是一个表达式：My name is {this.formatName({firstName: '史诗', lastName: '王爵'})}</p>
+                        <p>2.3、函数方法是一个表达式：My name is {this.formatName({ firstName: '史诗', lastName: '王爵' })}</p>
                         <p>2.4、{jsx}</p>
                     </li>
                     <li>
@@ -76,8 +82,16 @@ export default class  extends Component {
                         <h2>4. 状态state</h2>
                         <p>4.1、更新状态：{this.state.time.toLocaleTimeString()}</p>
                     </li>
+                    <li>
+                        <h2>5. 生命周期</h2>
+                        <p>5.1、{this.state.componentWillMount}</p>
+                        <p>5.2、{this.state.componentDidMount}</p>
+                        <p>5.3、{this.state.componentWillReceiveProps}</p>
+                        <p>5.4、{this.state.shouldComponentUpdate}</p>
+                        <p>5.5、{this.state.componentWillUpdate}</p>
+                        <p>5.6、{this.state.componentDidUpdate}</p>
+                    </li>
                 </ul>
-
             </div>
         )
     }
--- 06React/01syntax/src/index.js
+++ 06React/01syntax/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+// import App from './App'
 // import Lifecycle from './Lifecycle'
+import Cart from './Cart'
 
-const $root = document.getElementById('root');
-ReactDOM.render(<App/>, $root);
-// ReactDOM.render(<Lifecycle/>, $root);
+const $root = document.getElementById('root')
+// ReactDOM.render(<App/>, $root)
+// ReactDOM.render(<Lifecycle/>, $root)
+ReactDOM.render(<Cart title='React购物车'/>, $root)
 
 // 动态渲染
 // React 只更新它需要更新的部分
