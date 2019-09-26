8b9d1a444d96549b1692914e5bee3bc75150180c
xusp <xusp@weizhenzhendeiMac-2.local>
2019-09-26 18:24:21 +0800

    高阶组件

--- 06React/01syntax/src/components/Hoc.js
+++ 06React/01syntax/src/components/Hoc.js
@@ -1,23 +1,47 @@
 import React, { Component } from 'react'
 
-const Course = props => {
-    return (
-        <div>{props.stage} -- {props.name}</div>
-    )
-}
+// const Course = props => {
+//     return (
+//         <div>{props.stage} -- {props.name}</div>
+//     )
+// }
 
 // 高阶组件是一个函数
-const CourseWIthName = Comp => {
+const WithName = Comp => {
     // 重写组件生命周期
     class InterComp extends Component {
         componentDidMount() {
             console.log('组件挂载')
-        } 
+        }
         render() {
-            return (<Comp {...this.props} name='React高阶组件'/ >)
+            return (<Comp {...this.props} name='React高阶组件' />)
         }
     }
     return InterComp
 }
 
-export default CourseWIthName(Course)
+// 高阶组件链式调用
+const WithLog = Comp => {
+    // 思考：采用装饰器写法，Comp.name为Course，与链式写法输出InterComp不一样，为什么？
+    console.log(Comp.name + '渲染了')
+    return props => <Comp {...props}></Comp>
+}
+
+// 高阶组件 
+// export default WithName(Course) 
+// 高阶组件的链式调用
+// export default WithLog(WithName(Course))
+// 高阶组件装饰器写法
+@WithName
+@WithLog
+class Course extends Component {
+    render() {
+        return (
+            <div>
+                {this.props.stage} - {this.props.name}
+            </div>
+        );
+    }
+}
+export default Course
+
