53836bdd1f645a6600a143deb74377c7869c9781
xusp <xusp@weizhenzhendeiMac-2.local>
2019-10-16 18:27:06 +0800

    express

--- 07Node/01basic/05express.js
+++ 07Node/01basic/05express.js
@@ -1,12 +1,29 @@
 const express = require('express')
-const fs = require('fs')
 
 const app = express()
+app.listen(8080)
 
-app.get('./user', (req, res) => {
-    res.statusCode = 200; // 请求成功
-    res.setHeader("Content-Type", "application/json");
-    res.write(JSON.stringify([{ name: "xu", age: 20 }]));
-    res.end();
+// express一直都是从上往下走的
+
+// res可以发送任何格式的数据
+app.get('/index', (req, res, next) => {
+    console.log('test')
+    next()
 })
-app.listen(3000)
+// 可以分段处理同一个接口
+app.get('/index', (req, res, next) => {
+    res.send({ message: 'get' })
+})
+// 处理post请求[不带next参数]
+app.post('/index', (req, res) => {
+    res.send({ message: 'post' })
+})
+
+// 处理数据
+// get请求，直接req.query获取
+app.get('/get', (req, res, next) => {
+    res.send(req.query);
+})
+
+// 托管静态文件，使用 Express 中的 express.static 内置中间件函数
+app.use(express.static('www'))
--- 07Node/note.md
+++ 07Node/note.md
@@ -5,7 +5,14 @@
     - 自定义模块
         - 导出: module.exports
         - 导入: require
-    
-2. 其它
+2. 核心API
+    - buffer: 八位字节组成的[类]数组，可以有效的在js中存储二进制数据的缓存区。用十六进制表示
+    - fs, http, stream
+3. 其它
     -  `npm i nodemon -g`: 在每次修改js文件需要重新执行才能生效，安装nodemon可以监视文件改动，自动重启
     - 调试
+# 二、框架
+1. express
+    - 基本语法
+    
+2. koa
