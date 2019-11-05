f0fc3da46e905b61f4cb67dd2d24a46e62a332b2
xusp <xusp@weizhenzhendeiMac-2.local>
2019-11-05 11:14:15 +0800

    利用websocket实现简易聊天室功能

--- 07Node/02networkProgramming/03socket/chat.js
+++ 07Node/02networkProgramming/03socket/chat.js
@@ -4,6 +4,28 @@ const httpServer = http.createServer()
 httpServer.listen(3000)
 const wsServer = io.listen(httpServer)
 
-wsServer.on('connection', socket => {
+const socketArr = []
+const mesArr = []
 
+wsServer.on('connection', socket => {
+    socketArr.push(socket)
+
+    socket.on('disconnect', () => {
+        const n = socketArr.indexOf(socket)
+        if (n !== -1) {
+            socketArr.splice(n, 1)
+        }
+    })
+
+    socket.on('sendMes', res => {
+        mesArr.push(res)
+        console.log('res', res)
+        console.log('socketArr', socketArr.length)
+        socketArr.forEach( socketItem => {
+            if (socketItem !== socket) {
+                console.log('mesArr', mesArr)
+                socketItem.emit('receiveMes', mesArr)
+            }
+        })
+    })
 })
