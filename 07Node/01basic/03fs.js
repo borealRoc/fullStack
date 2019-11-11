c716aa1e8ee245504a073d35af2d0e3eb0874061
xusp <xusp@weizhenzhendeiMac-2.local>
2019-11-11 17:52:48 +0800

    异步读取文件的三种写法

--- 07Node/01basic/03fs.js
+++ 07Node/01basic/03fs.js
@@ -1,36 +1,44 @@
 const fs = require('fs');
 
 
-
 // 同步读取
 const data = fs.readFileSync('./config.js');
 console.log('同步读取', data);
 // 异步读取
 fs.readFile('./config.js', (err, data) => {
     if (err) console.log(err)
-    else console.log('异步读取', data)
+    else console.log('异步读取文件回调函数写法', data)
 })
 // 测试代码
 console.log('测试代码')
 
 
-
 // 异步读取promise写法
-const {promisify} = require('util')
+const { promisify } = require('util')
 const readFile = promisify(fs.readFile)
-readFile('./config.js').then(data => {console.log('异步读取promise写法', data)})
+readFile('./config.js').then(data => console.log('异步读取文件promise写法', data))
 // v10.0
 // fs Promises API
-const {promises} = require('fs')
-promises.readFile('./config.js').then(data => {console.log('fs Promises API', data)})
+const { promises } = require('fs')
+promises.readFile('./config.js').then(data => console.log('fs Promises API写法', data))
 
 
-
+const readFilePromise = promises.readFile('./config.js')
 // generator
+const co = require('co')
+co(function* () {
+    var result = yield Promise.resolve(readFilePromise)
+    return result
+}).then(function (value) {
+    console.log('异步读取文件generator写法', value)
+}, function (err) {
+    console.error('异步读取文件generator写法', err.stack)
+})
 
 
 // async + await
-// (async () => {
-//     const data = await fs.readFile('./config.js')
-//     console.log('异步读取async + await写法', data)
-// })()
+const asyncReadFile = async () => {
+    const data = await readFilePromise
+    console.log('异步读取文件async + await写法', data)
+}
+asyncReadFile()
