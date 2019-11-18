b8a8ab39a939613d1e5a3a5b82780e30dc66fbb5
xusp <xusp@weizhenzhendeiMac-2.local>
2019-11-18 15:40:36 +0800

    vue原理解析

--- 05Vue/04theory/XVue.js
+++ 05Vue/04theory/XVue.js
@@ -8,10 +8,15 @@ class XVue {
         this.observe(this.$data)
 
         // 模拟一下watcher创建
-        new Watcher()
-        this.$data.name
-        new Watcher()
-        this.$data.person.age
+        // new Watcher()
+        // this.$data.name
+        // new Watcher()
+        // this.$data.person.age
+        new Compile(this.$options.el, this)
+        // 执行created
+        if (this.$options.created()) {
+            this.$options.created().call(this)
+        }
     }
     // 遍历$data对象所有的属性，监听每一个属性
     observe(data) {
@@ -24,19 +29,21 @@ class XVue {
     defineReactive(obj, key, val) {
         // 使用递归方式解决data数据嵌套问题
         this.observe(val)
-
+        
+        // 每个属性有一个dep
         const dep = new Dep()
-
+        // console.log('dep')
         Object.defineProperty(obj, key, {
             get() {
+                // 如果一个属性在view中出现几次，那么一个dep就对应有几个watcher
                 Dep.target && dep.addDep(Dep.target)
+                // console.log('watcher')
                 return val
             },
             set(newVal) {
                 if (newVal === val) return
                 val = newVal
                 dep.notify()
-                // console.log(`${key}属性更新了:${val}`)
             }
         })
     }
