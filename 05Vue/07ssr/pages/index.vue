f62997d24e8d7b07e691e7a9f662d88df9ddad94
xusp <xusp@weizhenzhendeiMac-2.local>
2019-12-20 18:09:37 +0800

    服务端渲染

--- 05Vue/07ssr/pages/index.vue
+++ 05Vue/07ssr/pages/index.vue
@@ -8,21 +8,8 @@
       <h2 class="subtitle">
         My bedazzling Nuxt.js project
       </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+      <div>
+          <nuxt-link to="/user">用户</nuxt-link>
       </div>
     </div>
   </div>
---
+++ 05Vue/07ssr/pages/user.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h3>我是用户首页</h3>
+        <el-button></el-button>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
