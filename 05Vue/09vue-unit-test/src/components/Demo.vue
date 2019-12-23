f017392ea2fd0e2e288bd9f9ca8bef4f53fe6520
xusp <xusp@weizhenzhendeiMac-2.local>
2019-12-23 16:06:22 +0800

    vue中的单元测试

---
+++ 05Vue/09vue-unit-test/src/components/Demo.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+    <span>{{ message }}</span>
+    <button @click="changeMsg">点击</button>
+    </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        message: 'vue-text'
+      }
+    },
+    created () {
+      this.message = '一开始的message'
+    },
+    methods:{
+        changeMsg(){
+            this.message = '改变后的message'
+        }
+    }
+  }
+</script>
