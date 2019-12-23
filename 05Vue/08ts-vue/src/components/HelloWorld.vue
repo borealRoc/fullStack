b5f618cb2ed8bfc693d4865b190fafdb7ca95227
xusp <xusp@weizhenzhendeiMac-2.local>
2019-12-23 16:53:26 +0800

    使用ts写vue

---
+++ 05Vue/08ts-vue/src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class HelloWorld extends Vue {
+  // private 仅当前类可用
+  // protected 子类也可以用
+  // public  都可以用
+  @Prop() private msg!: string;
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
