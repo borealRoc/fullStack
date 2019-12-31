7873f921d5c1697f4fedcdcf632656e3abaedb08
xusp <xusp@weizhenzhendeiMac-2.local>
2019-12-31 18:10:46 +0800

    整合vuex

--- 05Vue/07ssr/pages/data/vuex.vue
+++ 05Vue/07ssr/pages/data/vuex.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    用户列表
-    <p @click="increment">计数: {{counter}}</p>
+    <h3>用户列表</h3>
+    <el-button @click="increment">{{counter}}</el-button>
     <p>
       <input type="text" placeholder="添加用户" @keyup.enter="add($event.target.value)" />
     </p>
@@ -23,11 +23,11 @@ function getUsers() {
 }
 
 export default {
-//   fetch() {
-//     // fetch在创建组件前执行填充状态树
-//     // 提交时注意命名空间
-//     return getUsers().then(users => this.$store.commit("users/set", users));
-//   },
+  fetch({store}) {
+    // fetch在创建组件前执行填充状态树
+    // 提交时注意命名空间
+    return getUsers().then(users => store.commit("users/set", users));
+  },
   computed: {
     ...mapState(["counter"]),
     ...mapState("users", ["list"])
