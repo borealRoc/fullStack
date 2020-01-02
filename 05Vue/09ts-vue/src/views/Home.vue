e139dc38d99b436cec27afb888971a1d8273dff8
xusp <xusp@weizhenzhendeiMac-2.local>
2020-01-02 10:01:16 +0800

    修改项目名称

---
+++ 05Vue/09ts-vue/src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="TypeScript基本语法" @add-list="onAddFeature" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld, {Feature} from '../components/HelloWorld.vue';
+
+@Component({
+  components: {
+    HelloWorld
+  },
+})
+export default class App extends Vue {
+  onAddFeature(feature:Feature){
+    console.log(feature.name);
+  }
+}
+</script>
