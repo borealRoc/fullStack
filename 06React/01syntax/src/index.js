c36a843a784b512e01b2f624bcc6e5291244425e
xusp <xusp@weizhenzhendeiMac-2.local>
2020-01-06 18:17:05 +0800

    企图解决一些无关痛痒的报错但没解决掉

--- 06React/01syntax/src/index.js
+++ 06React/01syntax/src/index.js
@@ -1,15 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-// eslint-disable-next-line
 import App from './App'
-// eslint-disable-next-line
 import Lifecycle from './Lifecycle'
-// eslint-disable-next-line
 import Cart from './Cart'
-import Message from './components/pureComponent';
-import Composition from './components/Composition';
-import Hoc from './components/Hoc';
-import ContextSample from './components/ContextSample';
+import pureComponent from './components/pureComponent'
+import Hoc from './components/Hoc'
+import Composition from './components/Composition'
+import ContextSample from './components/ContextSample'
 
 
 
@@ -19,11 +16,12 @@ const $root = document.getElementById('root')
 // 生命周期
 // ReactDOM.render(<Lifecycle/>, $root)
 // 纯组件
-// ReactDOM.render(<Message/>, $root)
+// ReactDOM.render(<pureComponent/>, $root)
 // 高阶组件
 // ReactDOM.render(<Hoc stage='React' />, $root)
 // 组件复合
 ReactDOM.render(<Composition/>, $root)
+// 组件跨层级通信 - 上下文
 // ReactDOM.render(<ContextSample/>, $root)
 
 // 动态渲染
